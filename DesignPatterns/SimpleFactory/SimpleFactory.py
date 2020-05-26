2c068efdfa3e2cf4969e924166186829016398d4
WadeIsShuaiB <WadeIsShuaiB@github.com>
2020-05-26 21:51:30 +0800

    1.添加简单工厂模式

---
+++ DesignPatterns/SimpleFactory/SimpleFactory.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+'''
+简单工厂模式
+'''
+
+
+class Animal(object):
+    def GetName(self):
+        raise NotImplementedError
+
+
+class Dog(Animal):
+    def GetName(self):
+        print('Dog')
+
+
+class Cat(Animal):
+    def GetName(self):
+        print('Cat')
+
+
+class AnimalsFactory(object):
+    def CreateAnimal(self, name):
+        if name == 'Dog':
+            return Dog()
+        elif name == 'Cat':
+            return Cat()
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    animalsFactory = AnimalsFactory()
+    animal = animalsFactory.CreateAnimal('Dog')
+    name = animal.GetName()
--- DesignPatterns/Singleton/Singleton.py
+++ DesignPatterns/Singleton/Singleton.py
@@ -3,28 +3,25 @@
 单例模式
 '''
 
-
 class Singleton(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
             Singleton._instance = object.__new__(cls)
         return Singleton._instance
 
-
 class A(Singleton):
     def __init__(self, param):
         self.txt = param
-
 
 class B(Singleton):
     def __init__(self, param):
         self.txt = param
 
-
-a = A('aaaa')
-print(a.txt)
-b = B('bbbb')
-print(b.txt)
-b.txt = '2222'
-print(a.txt)
-print(b.txt)
+if __name__ == '__main__':
+    a = A('aaaa')
+    print(a.txt)
+    b = B('bbbb')
+    print(b.txt)
+    b.txt = '2222'
+    print(a.txt)
+    print(b.txt)
